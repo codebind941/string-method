b54f13601635cc21b12cd90f19a176f15e7dda6e
Syed Saqib <soharwardiandco27@gmail.com>
2025-02-17 12:02:31 +0530

    numbers or integers method

---
+++ practicenumber.js
@@ -0,0 +1,14 @@
+function parseinteger(integer){
+    let value = parseInt(integer)
+    console.log(value)
+}
+
+parseinteger("34")
+
+
+function parsefloat(integer) {
+    let value1 = parseFloat(integer)
+    console.log(value1)
+}
+
+parsefloat("22,33,44")
